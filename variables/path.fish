9549be891621581b80da4c04670c5f896b0fde64
Nicholas Adamou <nicholasadamouemail@gmail.com>
2026-01-08 17:36:26 -0500

    Inlines path addition for early availability
    Defines an inline function to add paths to `$PATH`.
    
    This ensures that path additions work before external functions
    are loaded, which is necessary during early initialization.

--- variables/path.fish
+++ variables/path.fish
@@ -15,12 +15,23 @@ set -gx LINUX_PATHS /home/linuxbrew/.linuxbrew/bin /snap/bin
 # Reset PATH to default system paths
 set -gx PATH $DEFAULT_SYSTEM_PATHS
 
-# Add default paths (using add_paths function from functions/utilities/)
-add_paths $DEFAULT_PATHS
+# Helper function to add paths (defined inline since functions aren't loaded yet)
+function __add_paths_inline
+    for path in $argv
+        if test -d $path
+            if not contains $path $PATH
+                set -a PATH $path
+            end
+        end
+    end
+end
+
+# Add default paths
+__add_paths_inline $DEFAULT_PATHS
 
 # Add Linux-specific paths if on Linux
 if string match -r '^linux-gnu' (uname -s)
-    add_paths $LINUX_PATHS
+    __add_paths_inline $LINUX_PATHS
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
