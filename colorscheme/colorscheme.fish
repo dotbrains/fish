d1cfae76d2666a1b63b9e5833ae98d0a58049cff
Nicholas Adamou <nicholasadamouemail@gmail.com>
2026-01-09 06:03:52 -0500

    Enhance theme configuration for fish colorscheme
    Introduces a dynamic theme selection mechanism in the fish colorscheme file, allowing users to choose between 'nord' and 'gruvbox' themes. The default theme is set to 'gruvbox' if not specified. Additionally, it updates the FZF and Bat configurations based on the selected theme, improving user customization options.

--- colorscheme/colorscheme.fish
+++ colorscheme/colorscheme.fish
@@ -1,6 +1,10 @@
 # fish colorscheme
 
-set -g theme_color_scheme nord
+# Available themes: nord, gruvbox
+# Set FISH_THEME environment variable to change (default: gruvbox)
+set -q FISH_THEME; or set -g FISH_THEME gruvbox
+
+set -g theme_color_scheme $FISH_THEME
 set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
@@ -11,16 +15,37 @@ set -gx TERM xterm-256color
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# Nord for fzf
-# see: https://github.com/ianchesal/nord-fzf
-set -Ux FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS '
-    --color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1
-    --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1
-    --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac
-    --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b'
-
-# Nord for Bat
-set BAT_THEME "Nord"
+# Theme selection
+switch $FISH_THEME
+    case gruvbox
+        # Gruvbox Dark Medium for fzf
+        # see: https://github.com/morhetz/gruvbox
+        set -Ux FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS '
+            --color=fg:#d5c4a1,bg:#282828,hl:#fabd2f
+            --color=fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
+            --color=info:#83a598,prompt:#fb4934,pointer:#b8bb26
+            --color=marker:#8ec07c,spinner:#d3869b,header:#8ec07c'
+        
+        # Gruvbox for Bat
+        set BAT_THEME "gruvbox-dark"
+    
+    case nord
+        # Nord for fzf
+        # see: https://github.com/ianchesal/nord-fzf
+        set -Ux FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS '
+            --color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1
+            --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1
+            --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac
+            --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b'
+        
+        # Nord for Bat
+        set BAT_THEME "Nord"
+    
+    case '*'
+        echo "Warning: Theme '$FISH_THEME' not found. Available themes: nord, gruvbox"
+        echo "Defaulting to gruvbox..."
+        set -g FISH_THEME gruvbox
+end
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
